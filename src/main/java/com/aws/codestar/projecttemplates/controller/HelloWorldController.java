670df7e3c830a7638acca17351096c5186eb0d24
GUNGORDU Aydin <aydinbg@gmail.com>
2020-01-27 13:04:17 +0100

    Allow CORS

--- src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
@@ -2,15 +2,13 @@ package com.aws.codestar.projecttemplates.controller;
 
 import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Basic Spring web service controller that handles all GET requests.
  */
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/")
 public class HelloWorldController {
 
