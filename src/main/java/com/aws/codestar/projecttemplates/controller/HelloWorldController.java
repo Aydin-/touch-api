1e738ea9463ae0a1989dc9ee76468338979d6ca1
GUNGORDU Aydin <aydinbg@gmail.com>
2020-01-27 13:39:33 +0100

    Test

--- src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
@@ -41,6 +41,6 @@ public class HelloWorldController {
     @GetMapping(path = "/stream-flux", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<String> streamFlux() {
         return Flux.interval(Duration.ofSeconds(1))
-                .map(sequence -> "Flux - " + LocalTime.now().toString());
+                .map(sequence -> "" + isTouched);
     }
 }
